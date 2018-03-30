88836e9be096edfb1bcb9c50b3005d31f908a943
Maxime Augier <max@xolus.net>
2018-03-30 04:56:25 +0200

    Fill rectangles of text

--- Haskii/Text.hs
+++ Haskii/Text.hs
@@ -32,14 +32,32 @@ instance Transparent Text where
     breakTransparent = T.break
 
 -- | Splits a piece of text into several lines, and render them
--- | under each other, left-aligning their start location
+-- under each other, left-aligning their start location
 multiline :: Text -> Render Text
 multiline t = do
     (c,l) <- oneOf . zip [0..] . T.lines $ t
     drawAt (c,0) l
 
+-- | Fill a rectangle of given size, having the upper left corner at the origin.
+--
+-- Example:
+-- >>> Haskii.Text.putStrLn $ oneOf [(1,8,'.'), (3,5,'/'), (6,4,'#')] >>= ( \(p,s,c) -> move (p,p) >> fill c (s,s))
+-- <EMPTYLINE>
+--  ........
+--  ........
+--  ../////.
+--  ../////.
+--  ../////.
+--  ..///####
+--  ..///####
+--  .....####
+--       ####
+
+fill :: Char -> (Int,Int) -> Render Text
+fill c (height, width) = fromChunks [ (T.replicate width (T.singleton c),(l,0)) | l <- [0..height-1] ]
+
 -- | Perform the rendering operation and produce a single output
--- | suitable for printing directly.
+-- suitable for printing directly.
 render :: Render Text -> Lazy.Text
 render = Lazy.unlines . map Lazy.fromChunks . renderChunks
 
